2b1c39c025826cb9482a15b0426f1de539cc6287
Rohan Chari <rohan.rchari@gmail.com>
2025-03-19 00:03:00 -0400

    login flow to show UI if they email or password is not acceptable

--- authRoutes.js
+++ authRoutes.js
@@ -12,11 +12,12 @@ router.post("/register", verifyToken, async (req, res) => {
         const db = await connectToMongo();
         const userCollection = db.collection("users"); 
 
-        const { uid, email, emailVerified, providerData, createdAt, lastLoginAt } = req.body.user;
+        const { displayName, uid, email, emailVerified, providerData, createdAt, lastLoginAt } = req.body.user;
         let user = await userCollection.findOne({ _id: uid });
         if (!user) {
             user = {
                 _id: uid,
+                displayName: displayName,
                 email,
                 emailVerified,
                 createdAt: createdAt,
@@ -28,6 +29,32 @@ router.post("/register", verifyToken, async (req, res) => {
         }
 
         res.json({ message: "User registered successfully", user });
+    } catch (error) {
+        console.error("❌ Error registering user:", error);
+        res.status(500).json({ error: "Server error" });
+    }
+});
+
+router.post("/existing-user", async (req, res) => {
+    try {
+        const db = await connectToMongo();
+        const userCollection = db.collection("users"); 
+
+        const { email, displayName } = req.body;
+        let existingEmail = await userCollection.findOne({ email: email });
+        let existingDisplayName = await userCollection.findOne({ displayName: displayName });
+
+        if(existingEmail && existingDisplayName){
+            return res.status(400).json({message: "Email and Username are taken.", canRegister: false})
+        }
+        if(existingEmail){
+            return res.status(400).json({message: "Email is taken.", canRegister: false})
+        }
+        if(existingDisplayName){
+            return res.status(400).json({message: "Username is taken.", canRegister: false})
+        }
+        
+        return res.status(200).json({canRegister: true, message: "Successfully Registered!"})
     } catch (error) {
         console.error("❌ Error registering user:", error);
         res.status(500).json({ error: "Server error" });
